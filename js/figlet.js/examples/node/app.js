6862ef09e38a8039fda6fe796acab7b0d1b5175f
yyc <yyc@univalsoft.com>
2020-06-15 08:27:18 +0800

    Site updated: 2020-06-15 08:27:16

--- js/figlet.js/examples/node/app.js
+++ js/figlet.js/examples/node/app.js
@@ -1,3 +1,3 @@
-// build time:Sun Jun 14 2020 22:36:57 GMT+0800 (China Standard Time)
+// build time:Mon Jun 15 2020 08:27:14 GMT+0800 (China Standard Time)
 var figlet=require("../../lib/node-figlet.js");figlet("Hello World!","Standard",function(o,l){if(o){console.log("Something went wrong...");console.dir(o);return}console.log(l);figlet.text("Again, Hello World!","Graffiti",function(o,l){if(o){console.log("Something went wrong...");console.dir(o);return}console.log(l);figlet.text("Last time...",{font:"Standard",horizontalLayout:"full",verticalLayout:"full"},function(o,l){if(o){console.log("Something went wrong...");console.dir(o);return}console.log(l)})})});
 //rebuild by neat 
