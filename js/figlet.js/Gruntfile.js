393e3deacfd3315db56a08f93e8241daaf0cfa26
yyc <yyc@univalsoft.com>
2020-06-22 09:08:09 +0800

    Site updated: 2020-06-22 09:08:07

--- js/figlet.js/Gruntfile.js
+++ js/figlet.js/Gruntfile.js
@@ -1,3 +1,3 @@
-// build time:Mon Jun 22 2020 00:07:12 GMT+0800 (China Standard Time)
+// build time:Mon Jun 22 2020 09:08:05 GMT+0800 (China Standard Time)
 "use strict";module.exports=function(t){t.initConfig({jshint:{all:["Gruntfile.js","lib/*.js","<%= nodeunit.tests %>"],options:{jshintrc:".jshintrc"}},clean:{tests:["tmp"]},nodeunit:{tests:["test/*_test.js"]}});t.loadNpmTasks("grunt-contrib-jshint");t.loadNpmTasks("grunt-contrib-clean");t.loadNpmTasks("grunt-contrib-nodeunit");t.registerTask("test",["clean","nodeunit"]);t.registerTask("default",["jshint","test"])};
 //rebuild by neat 
