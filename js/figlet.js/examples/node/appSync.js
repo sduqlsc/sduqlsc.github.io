d975e041a3652919437315f8ed5a8b21872e5542
yyc <yyc@univalsoft.com>
2021-01-18 10:16:04 +0800

    Site updated: 2021-01-18 10:16:01

--- js/figlet.js/examples/node/appSync.js
+++ js/figlet.js/examples/node/appSync.js
@@ -1,3 +1,3 @@
-// build time:Mon Jan 18 2021 09:59:30 GMT+0800 (China Standard Time)
+// build time:Mon Jan 18 2021 10:15:59 GMT+0800 (China Standard Time)
 var figlet=require("../../lib/node-figlet.js");console.log(figlet.textSync("Hello World!","Standard"));console.log(figlet.textSync("Again, Hello World!","Graffiti"));console.log(figlet.textSync("Last time...",{font:"Standard",horizontalLayout:"full",verticalLayout:"full"}));
 //rebuild by neat 
