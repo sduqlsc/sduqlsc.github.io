b15d9d7cf5b5e5c46790204bc2a254ca0abdda89
yyc <yyc@univalsoft.com>
2021-01-25 22:29:23 +0800

    Site updated: 2021-01-25 22:29:16

--- js/figlet.js/Gruntfile.js
+++ js/figlet.js/Gruntfile.js
@@ -1,3 +1,3 @@
-// build time:Mon Jan 25 2021 21:09:15 GMT+0800 (China Standard Time)
+// build time:Mon Jan 25 2021 22:29:12 GMT+0800 (China Standard Time)
 "use strict";module.exports=function(t){t.initConfig({jshint:{all:["Gruntfile.js","lib/*.js","<%= nodeunit.tests %>"],options:{jshintrc:".jshintrc"}},clean:{tests:["tmp"]},nodeunit:{tests:["test/*_test.js"]}});t.loadNpmTasks("grunt-contrib-jshint");t.loadNpmTasks("grunt-contrib-clean");t.loadNpmTasks("grunt-contrib-nodeunit");t.registerTask("test",["clean","nodeunit"]);t.registerTask("default",["jshint","test"])};
 //rebuild by neat 
--- js/figlet.js/examples/node/appSync.js
+++ js/figlet.js/examples/node/appSync.js
@@ -1,3 +1,3 @@
-// build time:Mon Jan 25 2021 21:09:15 GMT+0800 (China Standard Time)
+// build time:Mon Jan 25 2021 22:29:12 GMT+0800 (China Standard Time)
 var figlet=require("../../lib/node-figlet.js");console.log(figlet.textSync("Hello World!","Standard"));console.log(figlet.textSync("Again, Hello World!","Graffiti"));console.log(figlet.textSync("Last time...",{font:"Standard",horizontalLayout:"full",verticalLayout:"full"}));
 //rebuild by neat 
