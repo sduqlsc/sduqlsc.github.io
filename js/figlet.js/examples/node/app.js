234921f98ca6cb1e42f4561e63108dcf252850ed
yyc <yyc@univalsoft.com>
2021-03-08 10:45:09 +0800

    Site updated: 2021-03-08 10:45:04

--- js/figlet.js/examples/node/app.js
+++ js/figlet.js/examples/node/app.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 02 2021 08:50:52 GMT+0800 (China Standard Time)
+// build time:Mon Mar 08 2021 10:45:03 GMT+0800 (China Standard Time)
 var figlet=require("../../lib/node-figlet.js");figlet("Hello World!","Standard",function(o,l){if(o){console.log("Something went wrong...");console.dir(o);return}console.log(l);figlet.text("Again, Hello World!","Graffiti",function(o,l){if(o){console.log("Something went wrong...");console.dir(o);return}console.log(l);figlet.text("Last time...",{font:"Standard",horizontalLayout:"full",verticalLayout:"full"},function(o,l){if(o){console.log("Something went wrong...");console.dir(o);return}console.log(l)})})});
 //rebuild by neat 
--- js/figlet.js/examples/node/appSync.js
+++ js/figlet.js/examples/node/appSync.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 02 2021 08:50:52 GMT+0800 (China Standard Time)
+// build time:Mon Mar 08 2021 10:45:03 GMT+0800 (China Standard Time)
 var figlet=require("../../lib/node-figlet.js");console.log(figlet.textSync("Hello World!","Standard"));console.log(figlet.textSync("Again, Hello World!","Graffiti"));console.log(figlet.textSync("Last time...",{font:"Standard",horizontalLayout:"full",verticalLayout:"full"}));
 //rebuild by neat 
